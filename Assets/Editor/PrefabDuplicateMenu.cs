6abde03f365f9e54bf22677f66e8d42a290af95a
zz12121212 <167679014@qq.com>
2026-01-24 10:36:33 +0800

    第二次提交

--- Assets/Editor/PrefabDuplicateMenu.cs
+++ Assets/Editor/PrefabDuplicateMenu.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public static class PrefabDuplicateMenu
 {
-    [MenuItem("GameObject/UI/CanvasPrefab", false)]
-    static void CreatCanvas() => SpawnPrefab("Assets/Prefabs/UIPrefabs/Canvas.prefab");
+    [MenuItem("GameObject/UI/PanelPrefab", false)]
+    static void CreatPanel() => SpawnPrefab("Assets/Prefabs/UIPrefabs/Panel.prefab");
 
     static void SpawnPrefab(string path) {
         var Prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
         if (!Prefab) {
-            Debug.LogError("没有Canvas预制体");
+            Debug.LogError("没有Panel预制体");
             return;
         }
         var Obj = PrefabUtility.InstantiatePrefab(Prefab) as GameObject;
--- Assets/Scripts/UISystem/UIManager.cs
+++ Assets/Scripts/UISystem/UIManager.cs
@@ -1,46 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+using UnityEngine.Events;
 
-public enum UIPanels {
-    StartUI,
-    EndUI,
-    PauseUI
-}
-
-
-
-public class UIManager : MonoBehaviour
-{
-    public static UIManager Instance;
-    private Dictionary<UIPanels,GameObject> UIObjects =  new Dictionary<UIPanels,GameObject>();
-
-    private void Start()
+    public enum UIPanels
     {
-        Instance = this;
-        UIPanelData[] panels = FindObjectsOfType<UIPanelData>();
-        foreach (UIPanelData data in panels) {
-            UIObjects.Add(data.panelType, data.gameObject);
-        }
-    }
-
-    public void OpenThePanel(UIPanels panel) {
-        if (!UIObjects[panel]) { return; }
-        bool NeedPause = UIObjects[panel].GetComponent<UIPanelData>().NeedPause;
-        if (NeedPause) {
-            Time.timeScale = 0;
-        }
-        UIObjects[panel].SetActive(true);
-    }
-
-    public void CloseThePanel(UIPanels panel) {
-        if (!UIObjects[panel]) { return; }
-        bool NeedPause = UIObjects[panel].GetComponent<UIPanelData>().NeedPause;
-        UIObjects[panel].SetActive(true);
-        if (NeedPause)
-        {
-            Time.timeScale = 1;
-        }
-    }
+        StartUI,
+        EndUI,
+        PauseUI,
+        EndingPanel,
+        emotionalKnot,
+        LoadPanel,
+        dialogUI
 }
+
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager Instance;
+        private Dictionary<UIPanels, GameObject> UIObjects = new Dictionary<UIPanels, GameObject>();
+
+        private void Start()
+        {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        UIPanelData[] panels = FindObjectsOfType<UIPanelData>();
+            foreach (UIPanelData data in panels)
+            {
+            if (!UIObjects.ContainsKey(data.panelType))
+            {
+                UIObjects.Add(data.panelType, data.gameObject);
+            }
+        }
+        }
+
+        public void OpenThePanel(int panelInt)
+        {
+            UIPanels panel = (UIPanels)panelInt;
+            if (!UIObjects[panel]) { return; }
+            bool NeedPause = UIObjects[panel].GetComponent<UIPanelData>().NeedPause;
+            if (NeedPause)
+            {
+                Time.timeScale = 0;
+            }
+            UIObjects[panel].SetActive(true);
+        }
+
+        public void CloseThePanel(int panelInt)
+        {
+            UIPanels panel = (UIPanels)panelInt;
+            if (!UIObjects[panel]) { return; }
+            bool NeedPause = UIObjects[panel].GetComponent<UIPanelData>().NeedPause;
+            UIObjects[panel].SetActive(false);
+            if (NeedPause)
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+
